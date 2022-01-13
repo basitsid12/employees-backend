55cd72b486bb7ee9ef212f05692b49b0decf843c
Abdul basit <siddiqiabdulbasit@gmail.com>
2022-01-13 15:12:35 +0500

    changes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const employees = require('./employeeRoute');
+const cors = require('cors')
 
 mongoose.connect('mongodb+srv://abdulbasit:basit123@cluster0.qbsti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 .then(() => console.log('Connected to mongoodb..'))
 .catch(err => console.log('Could not connect to mongodb', err))
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/employees', employees)
 app.get('/', (req, res) => {
     res.send('success')
